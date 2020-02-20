db2e6346b2f647e3360134647c7bb73322c940e2
Joel <Joel@DESKTOP-2GM3L3E>
2020-02-20 09:21:12 +0100

    intial

---
+++ IthsDemo-1/src/main/java/se/joeldegerman/ithsdemo/IthsDemo1Application.java
@@ -0,0 +1,13 @@
+package se.joeldegerman.ithsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthsDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IthsDemo1Application.class, args);
+	}
+
+}
---
+++ IthsDemo-1/src/test/java/se/joeldegerman/ithsdemo/IthsDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.joeldegerman.ithsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IthsDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
